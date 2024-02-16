b93cff264a2207eaae420b1b027017e87df0a57a
ぴーまん <ikmy1119924@gmail.com>
2024-02-16 15:35:16 +0900

    Fix page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import dayjs from "dayjs";
 import IntroModal from "@/components/IntroModal";
 import Table from "@/components/Table";
 import { flex } from "@shadow-panda/styled-system/patterns";
 import { cookies } from "next/headers";
 import { fetchScienceroomTable } from "@/services/scienceroom";
-import dayjs from "dayjs";
 
 export default async function Home({
   searchParams,
@@ -12,9 +12,9 @@ export default async function Home({
 }) {
   const key = cookies().get("hatoapi-key")?.value;
   const { y, m, d } = searchParams;
-  const date = dayjs(y && m && d ? [Number(y), Number(m) - 1, Number(d)] : new Date()).startOf(
-    "day"
-  );
+  const date = dayjs(y && m && d ? [Number(y), Number(m) - 1, Number(d)] : new Date())
+    .tz("Asia/Tokyo")
+    .startOf("day");
   const table = key
     ? await (
         await fetchScienceroomTable({
@@ -34,12 +34,11 @@ export default async function Home({
         h: "100dvh",
         justifyContent: "center",
         alignItems: "center",
+        overflow: "auto",
       })}
     >
       <IntroModal isOpen={!key} />
-      <div
-        className={flex({ w: "full", maxW: "breakpoint-xl", h: "full", p: 2, overflow: "auto" })}
-      >
+      <div className={flex({ w: "full", maxW: "breakpoint-xl", h: "full", p: 2 })}>
         <Table date={date.toDate()} table={table} />
       </div>
     </main>
